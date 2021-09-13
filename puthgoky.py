24e16ef1bdfca81d888fdbfb4aeb298d2b319d04
msy1717 <80758264+msy1717@users.noreply.github.com>
2021-09-13 11:53:20 +0530

    Update puthgoky.py

--- puthgoky.py
+++ puthgoky.py
@@ -5,10 +5,10 @@ import telebot
 ##TOKEN DETAILS
 TOKEN = "TRON"
 
-BOT_TOKEN = "1862943563:AAH1APY6rsAIGCarCfLLYLDsrof9xfXkUaE"
-PAYMENT_CHANNEL = "@AjayCoder" #add payment channel here including the '@' sign
+BOT_TOKEN = "1992537216:AAGU7Ir5B_rlKQNDM24KbhTRDHynaopr9zA"
+PAYMENT_CHANNEL = "@testing1717" #add payment channel here including the '@' sign
 OWNER_ID = "1194007250" #write owner's user id here.. get it from @MissRose_Bot by /id
-CHANNELS = ["@AjayCoder"] #add channels to be checked here in the format - ["Channel 1", "Channel 2"] 
+CHANNELS = ["@testing1717"] #add channels to be checked here in the format - ["Channel 1", "Channel 2"] 
               #you can add as many channels here and also add the '@' sign before channel username
 Daily_bonus = 0.001 #Put daily bonus amount here!
 Mini_Withdraw = 0.5  #remove 0 and add the minimum withdraw u want to set
