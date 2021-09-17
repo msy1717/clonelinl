70a4adfe3f9086bdb77eb22ac124a57337f302e9
msy1717 <80758264+msy1717@users.noreply.github.com>
2021-09-17 09:43:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ BOT_TOKEN = "1992537216:AAGU7Ir5B_rlKQNDM24KbhTRDHynaopr9zA"
 PAYMENT_CHANNEL = "@testing1717" #add payment channel here including the '@' sign
 OWNER_ID = "1194007250" #write owner's user id here.. get it from @MissRose_Bot by /id
 CHANNELS = ["@testing1717"] #add channels to be checked here in the format - ["Channel 1", "Channel 2"] 
-              #you can add as many channels here and also add the '@' sign before channel username
+lso add the '@' sign before channel username
 Daily_bonus = 0.001 #Put daily bonus amount here!
 Mini_Withdraw = 0.5  #remove 0 and add the minimum withdraw u want to set
 Per_Refer = 0.0001 #add per refer bonus here
